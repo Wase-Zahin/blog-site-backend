3b7fcd5ee0b4554b1f48458e8c2dd24f068af028
Wase-Zahin <w.zahin@yahoo.com>
2023-04-12 18:53:47 +0800

    best practices

--- server.py
+++ server.py
@@ -1,21 +1,22 @@
-from flask import Flask, jsonify, request, g
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 import sqlite3
+from threading import Lock
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r'/*': {'origins': '*'}})
 
-DATABASE = 'cms.db'
+lock = Lock()
 
 def get_db():
     db = getattr(g, '_database', None)
     if db is None:
-        db = g._database = sqlite3.connect(DATABASE)
+        db = g._database = sqlite3.connect("cms.db", check_same_thread=False)
         db.row_factory = sqlite3.Row
     return db
 
 @app.teardown_appcontext
-def close_db(error):
+def close_connection(exception):
     db = getattr(g, '_database', None)
     if db is not None:
         db.close()
@@ -23,42 +24,58 @@ def close_db(error):
 @app.route('/home', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
-        db = get_db()
-        cursor = db.execute("SELECT id, title, username, description FROM blogs")
-        res = [dict(row) for row in cursor.fetchall()]
-        return jsonify(res)
-    return jsonify([])
+        with lock:
+            cur = get_db().cursor()
+            sql = "SELECT id, title, username, description FROM blogs"
+            cur.execute(sql)
+            res = cur.fetchall()
+            if res:
+                return jsonify(res)
+    return jsonify()
 
-@app.route('/login', methods=['POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    username = request.json.get('username')
-    password = request.json.get('password')
-    db = get_db()
-    cursor = db.execute("SELECT id FROM users WHERE username = ? AND password = ?", (username, password))
-    res = cursor.fetchone()
-    if res is not None:
-        return jsonify({'status':'login_yes', 'id':res[0]})
+    if request.method == 'POST':
+        with lock:
+            cur = get_db().cursor()
+            username = request.json.get('username')
+            password = request.json.get('password')
+            sql = "SELECT * FROM users WHERE username = ? AND password = ?"
+            user = (username, password)
+            cur.execute(sql, user)
+            res = cur.fetchone()
+            if res:
+                userid = res['id']
+                return jsonify({'status':'login_yes', 'id':userid})
     return jsonify({'status': 'login_no'})
 
-@app.route('/create', methods=['POST'])
+@app.route('/create', methods=['GET', 'POST'])
 def create():
-    username = request.json.get('username')
-    title = request.json.get('title')
-    description = request.json.get('description')
-    db = get_db()
-    db.execute("INSERT INTO blogs (username, title, description) VALUES (?, ?, ?)", (username, title, description))
-    db.commit()
-    return jsonify({'status': 'success'})
+    if request.method == 'POST':
+        with lock:
+            cur = get_db().cursor()
+            username = request.json.get('username')
+            title = request.json.get('title')
+            description = request.json.get('description')
+            sql = "INSERT INTO blogs(username, title, description) VALUES (?, ?, ?);"
+            values = (username, title, description)
+            cur.execute(sql, values)
+            get_db().commit()
+            return ("Submitted successfully!")
+    return "Sorry, submission failed."
 
-@app.route('/description', methods=['POST'])
+@app.route('/description', methods=['GET', 'POST'])
 def detail():
-    id = request.json.get('id')
-    db = get_db()
-    cursor = db.execute("SELECT title, username, description FROM blogs WHERE id = ?", (id,))
-    res = cursor.fetchone()
-    if res is not None:
-        return jsonify(dict(res))
-    return jsonify({'status': 'not_found'})
+    if request.method == 'POST':
+        with lock:
+            cur = get_db().cursor()
+            id = request.json.get('id')
+            sql = "SELECT title, username, description FROM blogs WHERE id = ?"
+            cur.execute(sql, (id,))
+            res = cur.fetchone()
+            if res:
+                return jsonify(res)
+    return "Sorry the content is not available"
 
 if __name__ == '__main__':
-    app.run(debug=False, port=8000)
+    app.run(host='0.0.0.0', port=8000)
